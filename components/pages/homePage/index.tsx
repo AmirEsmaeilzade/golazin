f22c0d50543bf4ae0d241f66618107e13b02c979
AmirEsmaeilzade <d.am.8.ir.w@gmail.com>
2023-12-28 22:05:30 +0330

    create folders and install pakages

---
+++ components/pages/homePage/index.tsx
@@ -0,0 +1,14 @@
+import Button from '@mui/material/Button';
+
+const HomePage = () => {
+    return (
+        <>
+            <div className="text-zinc-500">
+                <p className='text-red-600'>yyhh</p>
+                <Button className='w-80' variant="contained">Contained</Button>
+            </div>
+        </>
+    )
+}
+
+export default HomePage;
